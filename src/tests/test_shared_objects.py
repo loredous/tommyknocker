2edd50d0a56816747e23e20391ea994581d3310b
Jeremy Banker <loredous@loredous.net>
2024-01-13 17:09:10 +0000

    add tests

--- src/tests/test_shared_objects.py
+++ src/tests/test_shared_objects.py
@@ -1,7 +1,7 @@
 import datetime
 from uuid import uuid4
-from shared.models.objects import Knocker, Knock
-from shared.models.dbobjects import DBKnocker, DBKnock
+from shared.models.objects import Knocker, Knock, Runner
+from shared.models.dbobjects import DBKnocker, DBKnock, DBRunner
 
 def test_knocker_from_db():
     db_knocker = DBKnocker(
@@ -17,3 +17,18 @@ def test_knocker_from_db():
     assert knocker.id == db_knocker.id
     assert knocker.last_seen == db_knocker.last_seen
 
+def test_runner_from_db():
+    db_runner = DBRunner(
+        name="test_runner",
+        description="test_description",
+        id=uuid4(),
+        image_name="test_image_name",
+        image_tag="test_image_tag"
+    )
+    runner = Runner.from_db(db_runner)
+    assert isinstance(runner, Runner)
+    assert runner.name == db_runner.name
+    assert runner.description == db_runner.description
+    assert runner.id == db_runner.id
+    assert runner.image_name == db_runner.image_name
+    assert runner.image_tag == db_runner.image_tag
